7441c7ee40f1d5f067ba958bf51a40d1cf6669ec
Zahar Elistratov <zaharelistratov12@gmail.com>
2023-06-28 17:44:38 +0400

    first commit

---
+++ src/components/screens/Home/Home.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+import styles from './Home.module.scss';
+
+const Home = () => {
+	return (
+		<div>
+			<Head>
+				<title>ASale</title>
+			</Head>
+		</div>
+	);
+};
+
+export default Home;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import {Inter} from 'next/font/google'
+
+import '@/styles/global.scss'
+
+const inter = Inter({
+	weight: ['400', '500', '600', '700', '800', '900'],
+	subsets: ['latin', 'cyrillic'],
+	display: 'swap',
+	style: 'normal',
+	variable: '--font-inter'
+})
+
+export default function App({Component, pageProps}: AppProps) {
+	return (
+		<div className={inter.variable}>
+			<Component {...pageProps} />
+		</div>
+	);
 }
